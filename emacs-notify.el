fb98f5739d8e8bfaab89419005a2cf177c29b1e5
Andy Holt <andrew.holt@hotmail.co.uk>
2013-12-29 18:58:28 +0000

    Setup file with boilerplate text.

---
+++ emacs-notify.el
@@ -0,0 +1,14 @@
+;;; emacs-notify.el --- a desktop notification system for emacs in ubuntu
+
+;; Author: Andy Holt (andrew.holt@hotmail.co.uk)
+;; Date: Sun 29 Dec 2013 18:55
+
+;;; Commentary
+
+;; Display notifications to the desktop using notify-send
+
+
+
+(provide 'emacs-notify)
+
+;;; emacs-notify.el ends here
